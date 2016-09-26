bbcc84b069fbaa26e55796b45e82cd267a75976d
Luis Galan Ramil <lgalanra@gamma25.aulas.gsyc.urjc.es>
2016-09-26 10:39:19 +0200

    Esqueleto programa

---
+++ calcoo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+class Calculadora():
+
+	def suma:
+		return
+	def resta:
+		return 
+
+
+if __name__ == "__main__": 
+	
+	try:
+		op1 = int(sys.argv[1])
+		op2 = int(sys.argv[2])
+	except ValueError:
+		sys.exit("Introducir numeros")
+		
+	
+	calculadora = Calculadora(op1,op2)	
+	
+  
+
