8b6c73b4897f6dafdc5c548aacd6f45a0ea2b68a
Luis Galan Ramil <lgalanra@gamma35.aulas.gsyc.urjc.es>
2016-09-26 19:46:06 +0200

    errores

--- calcplusplus.py
+++ calcplusplus.py
@@ -8,34 +8,34 @@ import calcoohija
 
 if __name__ == "__main__":
 
-texto = sys.argv[1]
+    texto = sys.argv[1]
 
-with open(texto) as d
-    lineas = csv.lines(d)
+with open(texto) as d:
+    lines = csv.reader(d)
     
-    for fila in lineas:
-        line = row.split(',')
+    for fila in lines:
+        line = fila.split(',')
         operador = line[0]
         resultado = int(line[1])
         
         if operador == "suma":
-            for i in range(2,len(line))
+            for i in range(2,len(line)):
                 resultado = resultado + int(line[i])
             print(resultado)
             
         elif operador == "resta":
-            for i in range(2,len(line))
+            for i in range(2,len(line)):
                 resultado = resultado - int(line[i])
             print(resultado)
         
         elif operador == "multiplica":
-            for i in range(2,len(line))
+            for i in range(2,len(line)):
                 resultado = resultado * int(line[i])
             print(resultado)
             
-        elif operador == "divide"
-            for i in range(2,len(line))
-                if int(line[i] != 0:
+        elif operador == "divide":
+            for i in range(2,len(line)):
+                if int(line[i]) != 0:
                     resultado = resultado / int(line[i])
                 else:
                     sys.exit("División por cero no válida")
