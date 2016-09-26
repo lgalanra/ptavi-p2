1f227bbe11482df486abbabd10135bed3657e378
Luis Galan Ramil <lgalanra@gamma35.aulas.gsyc.urjc.es>
2016-09-26 18:24:13 +0200

    hacer switch

--- calcplus.py
+++ calcplus.py
@@ -4,4 +4,40 @@
 import sys
 import calcoohija
 
+if __name__ == "__main__":
 
+    texto = open(sys.argv[1])
+    lineas = texto.readlines()
+    
+    for linea in lineas:
+    
+        print(linea, len(linea))
+        line = linea.split(',')
+        operando = line[0]
+        newline = line[1:]
+        print(newline)
+        
+        print(operando)
+        
+        if operando == "suma":
+            resultado = int(line[1])
+            
+            for newline in line:
+                resultado = resultado + 1
+            print(resultado)
+            
+        elif operando == "resta":
+            resultado = int(line[1])
+            
+        elif operando == "multiplica":
+            resultado = int(line[1])
+            
+        elif operando == "divide":
+            resultado = int(line[1])
+            
+        else:
+            sys.exit("bye")
+        
+        
+                
+    texto.close()
