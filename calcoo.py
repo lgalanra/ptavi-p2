9c56cea6bb8fef587baaac2930bf820ac0cdbcbe
Luis Galan Ramil <lgalanra@gamma35.aulas.gsyc.urjc.es>
2016-09-26 16:31:55 +0200

    hecho

--- calcoo.py
+++ calcoo.py
@@ -8,22 +8,22 @@ class Calculadora():
     def __init__(self, value1, value2):
         self.op1 = value1
         self.op2 = value2
-        
-    def suma:
-    	return op1 + op2
-    def resta:
-    	return op1 - op2
+          
+    def suma(self):
+    	return self.op1 + self.op2
+    def resta(self):
+    	return self.op1 - self.op2
 
 
 if __name__ == "__main__": 
 	
-	try:
-    	value1 = int(sys.argv[1])
-    	value2 = int(sys.argv[3])
+    try:
+        value1 = int(sys.argv[1])
+        value2 = int(sys.argv[3])
     except ValueError:
     	sys.exit("Introducir solo numeros")
 		
-    calculadora = Calculadora(value1,value2)	
+    calculadora = Calculadora(value1,value2)
 		
     if sys.argv[2] == "suma":
         print(calculadora.suma())
